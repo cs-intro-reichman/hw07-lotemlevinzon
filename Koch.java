28936c60e5fa9886223f193534ff7fe33f4ea9e3
Lotem Levinzon <lotem.levinzon@post.runi.ac.il>
2025-12-27 15:22:05 +0200

    Completed assignment

--- Koch.java
+++ Koch.java
@@ -27,8 +27,22 @@ public class Koch {
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		if (n == 0 ) {
+       StdDraw.line(x1, y1, x2, y2);
+	   return;
+
+		}
+		double sqrt3 = Math.sqrt(3);
+		double xp1 =  x1 + (x2 - x1)/3;
+		double yp1 =  y1 + (y2 - y1)/3;
+		double xp2 = x1 + 2*(x2 - x1)/3;
+        double yp2= y1 + 2*(y2 - y1)/3;
+		double xp3 = sqrt3/6 * (y1 - y2) + 0.5 * (x1 +x2);
+		double yp3 = sqrt3/6 * (x2 - x1) + 0.5 * (y1 +y2);
+		curve(n-1, x1, y1, xp1, yp1);
+		curve(n-1, xp1, yp1, xp3, yp3);
+		curve(n-1, xp3, yp3, xp2, yp2);
+		curve(n-1, xp2, yp2, x2, y2);
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
@@ -36,7 +50,15 @@ public class Koch {
 		// A little tweak that makes the drawing look better
 		StdDraw.setYscale(0, 1.1);
 		StdDraw.setXscale(0, 1.1);
-		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		double x1 = 0.1;
+		double x2 = 0.9;
+		double y1 = 0.3;
+		double y2 = 0.3;
+		double sqrt3 = Math.sqrt(3);
+		double x3 = sqrt3/6 * (y1 - y2) + 0.5 * (x1 +x2);
+		double y3 = sqrt3/6 * (x2 - x1) + 0.5 * (y1 +y2);
+		curve(n, x1, y1, x2, y2);
+    curve(n, x2, y2, x3, y3);
+    curve(n, x3, y3, x1, y1);
 	}
 }
