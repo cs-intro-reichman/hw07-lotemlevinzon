256e1aacbc41c0aa7f1d4e4a4a12b4ff26584cc6
Lotem Levinzon <lotem.levinzon@post.runi.ac.il>
2025-12-27 15:44:19 +0200

    Completed assignment

--- IsSorted.java
+++ IsSorted.java
@@ -7,8 +7,16 @@ public class IsSorted {
 
     // Helper recursive function
     private static boolean isSorted(int[] a, int i) {
-        //// Replace the following statement with your code
-        return false; 
+        if (a.length == 0 || a.length == 1) {
+            return true;
+        }
+        if (i >= a.length - 1) {
+    return true;  
+}
+        if (a[i] > a[i +1]){
+         return false; 
+        }
+        return isSorted(a, i+1);
     }
 
     public static void main(String[] args) {
